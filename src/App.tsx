0201172ec6f952bcbe28c30390dfbc00ebc8aa81
NathanielBrewer <nathanielc.brewer@gmail.com>
2024-03-14 19:54:43 -0300

    feat: test text operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+import { BackendCommunicationClient } from './common/backendCommunicationClient';
 
 function App() {
+  let backendCommunicationClient = BackendCommunicationClient.getInstance();
+
+  useEffect(() => {
+    (async function () {
+      let result = await backendCommunicationClient.postText('test text');
+      console.log('result', result);
+      let testAddResult = await backendCommunicationClient.getText('0Q4KYqugnsUmVMvpV8QS');
+      console.log('testAddResult', testAddResult);
+    })()
+  })
   return (
     <div className="App">
       <header className="App-header">
