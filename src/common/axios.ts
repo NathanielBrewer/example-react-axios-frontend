b2080571b965926b243cc40dfebfe61458607866
NathanielBrewer <nathanielc.brewer@gmail.com>
2024-03-18 14:05:10 -0300

    fix: set production baseURL

--- src/common/axios.ts
+++ src/common/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import config from '../config';
 
+// HACK: should be using env vars to establish an environment-dependent config
 const axiosInstance = axios.create({
-  baseURL: config.backendURL,
+  baseURL: 'https://vast-cove-24733-f2209d5e3910.herokuapp.com/',
 });
 
 export default axiosInstance;
